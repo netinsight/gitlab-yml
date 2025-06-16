ae1e42a6153cbe0d4e9cd0d7eb6a19b2b06dd03d
koooge <koooooge@gmail.com>
2025-06-16 10:29:02 +0200

    feat: remove undocumented rules:start_in
    Signed-off-by: koooge <koooooge@gmail.com>

--- src/config/types/rules.ts
+++ src/config/types/rules.ts
@@ -5,27 +5,23 @@ import { WhenOptions } from ".";
  */
 type RulesDefinition<T = WhenOptions> = Array<{
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#rulesif
+     * @see https://docs.gitlab.com/ci/yaml/#rulesif
      */
     if?: string;
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#ruleschanges
+     * @see https://docs.gitlab.com/ci/yaml/#ruleschanges
      */
     changes?: string[];
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#rulesexists
+     * @see https://docs.gitlab.com/ci/yaml/#rulesexists
      */
     exists?: string;
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#permitted-attributes
+     * @see https://docs.gitlab.com/ci/yaml/#ruleswhen
      */
     when?: T;
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#permitted-attributes
-     */
-    start_in?: string;
-    /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#rulesallow_failure
+     * @see https://docs.gitlab.com/ci/yaml/#rulesallow_failure
      */
     allow_failure?: boolean;
 }>;
