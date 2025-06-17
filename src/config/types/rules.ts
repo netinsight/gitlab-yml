42b7f94f4d9c456b0015f7e4a5dc3cf2bf668c3b
koooge <koooooge@gmail.com>
2025-06-17 19:37:35 +0200

    feat: Update rules (#19)
    * feat: remove undocumented rules:start_in
    
    Signed-off-by: koooge <koooooge@gmail.com>
    
    * feat: add rules:needs, variables, and interruptible
    
    Signed-off-by: koooge <koooooge@gmail.com>
    
    * chore: bump 0.4.6
    
    Signed-off-by: koooge <koooooge@gmail.com>
    
    ---------
    
    Signed-off-by: koooge <koooooge@gmail.com>

--- src/config/types/rules.ts
+++ src/config/types/rules.ts
@@ -1,33 +1,43 @@
-import { WhenOptions } from ".";
+import type { NeedsDefinition } from "./needs";
+import type { VariablesDefinition } from "./variables";
+import type { WhenOptions } from "./when";
 
 /**
  * @see https://docs.gitlab.com/ee/ci/yaml/#rules
  */
 type RulesDefinition<T = WhenOptions> = Array<{
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#rulesif
+     * @see https://docs.gitlab.com/ci/yaml/#rulesif
      */
     if?: string;
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#ruleschanges
+     * @see https://docs.gitlab.com/ci/yaml/#ruleschanges
      */
     changes?: string[];
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#rulesexists
+     * @see https://docs.gitlab.com/ci/yaml/#rulesexists
      */
     exists?: string;
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#permitted-attributes
+     * @see https://docs.gitlab.com/ci/yaml/#ruleswhen
      */
     when?: T;
     /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#permitted-attributes
-     */
-    start_in?: string;
-    /**
-     * @see https://docs.gitlab.com/ee/ci/yaml/#rulesallow_failure
+     * @see https://docs.gitlab.com/ci/yaml/#rulesallow_failure
      */
     allow_failure?: boolean;
+    /**
+     * @see https://docs.gitlab.com/ci/yaml/#rulesneeds
+     */
+    needs?: NeedsDefinition;
+    /**
+     * @see https://docs.gitlab.com/ci/yaml/#rulesvariables
+     */
+    variables?: VariablesDefinition;
+    /**
+     * @see https://docs.gitlab.com/ci/yaml/#rulesinterruptible
+     */
+    interruptible?: boolean;
 }>;
 
 export { RulesDefinition };
